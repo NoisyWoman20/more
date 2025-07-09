74e301c0c6cd36b06d958183dd16961e29e6ad60
NoisyWoman20 <w090520wasd@outlook.com>
2025-07-09 23:10:42 +0800

    Update ModRecipeSerializers.java

--- src/main/java/com/noisy_woman_20/more/recipe_serializer/ModRecipeSerializers.java
+++ src/main/java/com/noisy_woman_20/more/recipe_serializer/ModRecipeSerializers.java
@@ -13,7 +13,7 @@ import net.minecraft.util.Identifier;
 public class ModRecipeSerializers {
 	public static void registerModRecipeSerializers() {    // 配方序列化器注册
 		registerRecipeSerializer(TOTEM_OF_DYING, TotemOfDyingRecipeSerializer.ID);
-		More.LOGGER.info("Registered mod recipe serializer successfully");
+		More.LOGGER.info("Registered mod recipe serializers successfully");
 	}
 
 	private static <T extends Recipe<?>> void registerRecipeSerializer(RecipeSerializer<T> serializer, String Id) {
